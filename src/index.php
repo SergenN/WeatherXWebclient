58f513fb7ca806cb6e73d545dc074d6bc8d605b4
Sergen Nurel <sergen_nurel@hotmail.com>
2015-10-28 17:40:34 +0100

    fixed index height of Iframe

--- src/index.php
+++ src/index.php
@@ -17,11 +17,5 @@ $title = "Home";
 
 <?php include_once 'includes/header.php'; ?>
 <?php include_once 'includes/navbar.php'; ?>
-
-    <div class="row">
-            <div class="col-md-12">
-                <iframe width="100%" height="700px" frameBorder="0" src="http://umap.openstreetmap.fr/nl/map/untitled-map_56362?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe>
-            </div>
-    </div>
-
+<iframe width="100%" height="535vh" frameBorder="0" src="http://umap.openstreetmap.fr/nl/map/untitled-map_56362?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe>
 <?php include_once 'includes/footer.php'; ?>
