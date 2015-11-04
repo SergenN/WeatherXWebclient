b5fedca977fbee550b4bda457f3a8ca924e229c9
Leon Wetzel <leonwetzel@gmail.com>
2015-11-04 13:14:51 +0100

    x

--- src/other/js/Map.js
+++ src/other/js/Map.js
@@ -1,55 +1,99 @@
 /**
- * Created by Leon on 3-11-2015.
+ * Created by Sergen on 29-10-2015.
  */
+/* chart functions */
 
-$(document).ready(function(){
+google.load('visualization', '1', {packages: ['line', 'corechart']});
+google.setOnLoadCallback(drawChart);
 
-    var height = $(document).height();
-    var canvas = $("#map_canvas");
-    canvas.height(height - 60);
-    canvas.width("100%");
+function drawChart() {
+    var airPressData = google.visualization.arrayToDataTable([
+        ['Seconds', 'Sea level', 'Station level'],
+        ['1',  1034.5, 1007.5],
+        ['2',  1033, 1008],
+        ['3',  1030, 1009.9],
+        ['4',  1032, 1008],
+        ['5',  1030, 1010],
+        ['6',  1031, 1008.3],
+        ['7',  1029, 1009],
+        ['8',  1028, 1007]
+    ]);
 
-
-    var gmarkers = [];
-
-    var options = {
-        zoom: 2,
-        center: new google.maps.LatLng(53.2406868, 6.5316302), // centered Zernikeplein
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        mapTypeControl: false,
-        scaleControl: true
+    var airPressOptions = {
+        title: 'Millibars',
+        curveType: 'function',
+        legend: { position: 'bottom' }
     };
 
-    var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+    var airPressChart = new google.visualization.LineChart(document.getElementById('airpress_div'));
 
+    airPressChart.draw(airPressData, airPressOptions);
 
+    var precipitationData = google.visualization.arrayToDataTable([
+        ['Seconds', 'Amount of snow', 'Precipitation'],
+        ['1',  30, 28],
+        ['2',  31, 30],
+        ['3',  29, 29],
+        ['4',  33, 29],
+        ['5',  33, 30],
+        ['6',  32, 28],
+        ['7',  33, 27],
+        ['8',  31, 30]
+    ]);
 
-    $.getJSON( "other/data/stations.json", function( data ) {
-        $.each( data, function( key, val ) {
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(val.latitude, val.longitude),
-                map: map,
-                title: val.name,
-                id: key
-            });
+    var precipitationOptions = {
+        title: 'Centimeters',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
 
-            (function(marker, key) {
-                google.maps.event.addListener(marker, 'click', function() {
-                    var url = window.location.href;
-                    url = url.replace("index.php", "");
-                    url = url+"station.php?id="+marker.id;
-                    var win = window.open(url, "_blank");
-                    win.focus();
-                });
-            })(marker, key);
+    var precipitationChart = new google.visualization.LineChart(document.getElementById('precipitation_div'));
 
-            gmarkers.push(marker);
-        });
-        var markerCluster = new MarkerClusterer(map, gmarkers);
-    });
-});
+    precipitationChart.draw(precipitationData, precipitationOptions);
 
+    var dewpData = google.visualization.arrayToDataTable([
+        ['Seconds', 'Temperature', 'Dew point'],
+        ['1',  30, 1],
+        ['2',  31, 0],
+        ['3',  32, -5],
+        ['4',  29, 3],
+        ['5',  30, 1],
+        ['6',  31, 0],
+        ['7',  32, -5],
+        ['8',  29, 3]
+    ]);
 
+    var dewpOptions = {
+        title: 'Temperatures',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
 
+    var dewpChart = new google.visualization.LineChart(document.getElementById('temp_dewp_div'));
 
+    dewpChart.draw(dewpData, dewpOptions);
 
+    var visibData = google.visualization.arrayToDataTable([
+        ['Seconds', 'Visibility'],
+        ['1',  30],
+        ['2',  34],
+        ['3',  35],
+        ['4',  34],
+        ['5',  33],
+        ['6',  32],
+        ['7',  35],
+        ['8',  33]
+    ]);
+
+    var visibOptions = {
+        title: 'Kilometers',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var visibChart = new google.visualization.LineChart(document.getElementById('visib_div'));
+
+    visibChart.draw(visibData, visibOptions);
+}
+
+/* Socket options */
