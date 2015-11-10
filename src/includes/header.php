0b448bb1f626c2e246bcab342e48209c9ca79536
Sergen Nurel <sergen_nurel@hotmail.com>
2015-11-10 20:04:40 +0100

    implemented sweet alert, moved javascript files (not sure why?), minified libraries for faster loading speed.

--- src/includes/header.php
+++ src/includes/header.php
@@ -10,17 +10,24 @@
 <html>
     <head lang="en">
         <title>WeatherX - <?php echo $title; ?></title>
+        <!--Style sheets-->
         <link rel="stylesheet" href="other/css/bootstrap.min.css">
         <link rel="stylesheet" href="other/css/style.css">
-        <link rel="stylesheet" href="other/css/bootstrap-table.css">
+        <link rel="stylesheet" href="other/css/bootstrap-table.min.css">
         <link rel="stylesheet" href="other/css/bootstrap-switch.min.css">
+        <link rel="stylesheet" href="other/css/sweetalert.min.css">
 
         <?php if($user->getTheme() == 'green'){
             echo '<link rel="stylesheet" href="other/css/greenTheme.css">';
         }?>
 
-        <script type="text/javascript" src="other/js/jquery-2.1.4.min.js"></script>
-        <script type="text/javascript" src="other/js/bootstrap-table.js"></script>
-        <script type="text/javascript" src="other/js/bootstrap-switch.min.js"></script>
+        <!--Core libraries-->
+        <script src="other/js/jquery-2.1.4.min.js"></script>
+        <script src="other/js/bootstrap.min.js"></script>
+
+        <!--Dependent libraries-->
+        <script src="other/js/bootstrap-table.min.js"></script>
+        <script src="other/js/sweetalert.min.js"></script>
+        <script src="other/js/bootstrap-switch.min.js"></script>
     </head>
     <body>
